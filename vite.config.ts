d8a8f98d70732f665b13de4a42b893e4b05df68f
Winmix713 <takosadam@gmail.com>
2025-04-17 01:44:47 +0000

    chore: update lock files and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import macrosPlugin from "vite-plugin-babel-macros"; // 🔥 Itt a mágikus plugin
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
+    macrosPlugin(), // 💡 FONTOS: legyen az első plugin, hogy működjön styled-components/macro
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -30,6 +29,16 @@ export default defineConfig(({ mode }) => ({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@widgets": path.resolve(__dirname, "./src/widgets"),
       "@db": path.resolve(__dirname, "./src/db"),
+      "@features": path.resolve(__dirname, "./src/features"), // 💥 alias a todosSlice-hoz!
     },
   },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.(js|jsx|ts|tsx)$/,
+    exclude: [],
+  },
 }));
+
+
+
+
